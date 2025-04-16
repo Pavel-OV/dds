23e3c479683f0dcf2a26d23adaac08d954fb66ee
Pavel <black_yaguar@mail.ru>
2025-04-16 11:43:26 +0300

    создание views и пути path

--- d_d_s/site_ddsapp/urls.py
+++ d_d_s/site_ddsapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'site_ddsapp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_page/', views.create_page, name='create_page'),
+    path('edit_page/', views.edit_page, name='edit_page'),
+    
+    
 ]
--- d_d_s/site_ddsapp/views.py
+++ d_d_s/site_ddsapp/views.py
@@ -3,16 +3,43 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("""Тестовое задание: Веб-сервис для управления движением денежных средств (ДДС)\n Таблица с записями о ДДС.
-■ Фильтры по дате, статусу, типу, категории и подкатегории.""")
+    max_len = 25
+    # pk_lst = Recipes.objects.exclude(image__exact='').values_list('id', flat=True)
+    # if len(pk_lst) <= max_carussel_len:
+    #     pk_rnd = pk_lst
+    # else:
+    #     pk_rnd = choices(pk_lst, k=5)
+    # recipes_carussel = {}
+    # for k, i in enumerate(pk_rnd):
+    #     recipes_carussel[f'set{k}'] = Recipes.objects.filter(pk=i)
+#     return HttpResponse("""Добавление, редактирование и удаление статусов, типов,
+# категорий и подкатегорий, а также установление необходимых
+# зависимостей.""")
+
+    return render(request, "site_ddsapp/index.html", {'recipes_carussel': max_len})
+
+def edit_page(request):
+      max_len = 25
+      return render(request, "site_ddsapp/index.html", {'recipes_carussel': max_len})
+
+#      return HttpResponse("""Добавление, редактирование и удаление статусов, типов,
+# категорий и подкатегорий, а также установление необходимых
+# зависимостей.""")
+
+
+def create_page(request):
+     return HttpResponse("""Добавление, редактирование и удаление статусов, типов,
+категорий и подкатегорий, а также установление необходимых
+зависимостей.""")
 
 def directory_management(request):
     return HttpResponse("""Добавление, редактирование и удаление статусов, типов,
 категорий и подкатегорий, а также установление необходимых
 зависимостей.""")
 
-def creat_edit_entry(request):
-    return HttpResponse("""■ Форма для ввода данных.
-■ Поле выбора категорий, автоматически фильтрующее
-подкатегории на основе выбранной категории.""")
+# def creat_edit_entry(request):
+#     return HttpResponse("""■ Форма для ввода данных.
+# ■ Поле выбора категорий, автоматически фильтрующее
+# подкатегории на основе выбранной категории.""")
