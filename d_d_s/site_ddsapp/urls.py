fa1034947dcec0c61ae9f2da1f91f5889293d1e3
Pavel <black_yaguar@mail.ru>
2025-04-17 14:32:00 +0300

    добавлены пути ist_status, create_status, directory_management

--- d_d_s/site_ddsapp/urls.py
+++ d_d_s/site_ddsapp/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create_page/', views.create_page, name='create_page'),
     path('edit_page/', views.edit_page, name='edit_page'),
+    path('list_status/', views.list_status, name='list_status'),
+    path('create_status/', views.create_status, name='create_status'),
+    path('directory_management/', views.directory_management, name='directory_management'),
     
     
 ]
