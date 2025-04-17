f761021fe8e8c5238fa2700c41bbac57d1a960ea
Pavel <black_yaguar@mail.ru>
2025-04-17 14:30:04 +0300

    добавил STATIC

--- d_d_s/d_d_s/settings.py
+++ d_d_s/d_d_s/settings.py
@@ -115,6 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+# Пути к директориям с исходными статическими файлами
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # директория со статикой
+]
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'ctatic/'
 
